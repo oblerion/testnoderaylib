c2c9684df53ba229516e815c68261f6e43472371
oblerion <oblerion@protonmail.com>
2025-06-19 09:32:26 +0200

    test

---
+++ index.js
@@ -0,0 +1,16 @@
+const r = require('raylib')
+
+const screenWidth = 800
+const screenHeight = 450
+r.InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window")
+r.SetTargetFPS(60)
+
+while (!r.WindowShouldClose()) {
+    r.BeginDrawing();
+    r.ClearBackground(r.RAYWHITE)
+    r.DrawText("Congrats! You created your first node-raylib window!", 120, 200, 20, r.LIGHTGRAY)
+
+    r.DrawRectangle(23,23,100,20,r.BLUE);
+    r.EndDrawing()
+}
+r.CloseWindow()
